b0b921d6f3f9c4673d2f2a8299a0226be4a57082
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-06-02 16:14:31 +0900

    Slim down deploy package

--- lambda-prep.sh
+++ lambda-prep.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -O extglob
 
 if [ ! -d env ]; then
     LC_ALL=C virtualenv env
@@ -9,7 +9,8 @@ if [ -f lambda-deploy.zip ]; then
     rm lambda-deploy.zip
 fi
 
-zip lambda-deploy *.py *.json cjkvi-ids/ids.txt
+zip lambda-deploy *.py *.json cjkvi-ids/ids.txt -x \*.pyc
 
 cd env/lib/python2.7/site-packages
-zip -r ../../../../lambda-deploy .
+zip -r ../../../../lambda-deploy ./!(pip*|wheel*|setuptools*|easy_install*) \
+    -x \*.pyc
